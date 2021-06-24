20678f422b7300b76b88282438b737752807204e
Harshil <ilovebhagwan@gmail.com>
2021-06-25 02:28:51 +0530

    use powershell instead of tkinter for clipboard

--- get_clipboard.py
+++ get_clipboard.py
@@ -1,17 +1,24 @@
-from tkinter import Tk
 import re
+import logging
 import subprocess
 
 from typing import Optional
 
 
+def call_clipboard_cmd() -> str:
+    """Runs a powershell command to get clipboard commands. Returns an empty string if clipboard is empty."""
+    clipboard = subprocess.run(['powershell', '-Command', 'Get-Clipboard'], stdout=subprocess.PIPE)
+    copied = clipboard.stdout.decode().strip()
+    return copied
+
+
 def get_clipboard_content() -> Optional[str]:
-    root = Tk()
-    root.withdraw()
-    clipboard = root.clipboard_get()
-    num = re.search(r"\d+", clipboard)
+    """Fetches the first copied item from clipboard and returns the last number if found."""
+    copied = call_clipboard_cmd()
+    num = re.search(r"\d+", copied)
     return num.group() if num else None
 
 
 def purge_clipboard_contents() -> None:
-    subprocess.run(["powershell", "-Command", "scb"])    
+    subprocess.run(["powershell", "-Command", "scb"])
+    logging.debug('purging clipboard')
