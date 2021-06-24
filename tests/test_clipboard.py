9d13443b84a0417231c6e701f4c3f2e870f0a022
Harshil <37377066+harshil21@users.noreply.github.com>
2021-06-25 03:33:08 +0530

    Use logging, add tests, drop `tkinter` usage (#1)

---
+++ tests/test_clipboard.py
@@ -0,0 +1,22 @@
+import subprocess
+from re import sub
+
+import pytest
+
+from helpers.get_clipboard import get_clipboard_content, purge_clipboard_contents, call_clipboard_cmd
+
+
+@pytest.mark.parametrize("copied,expected", [('text (420) text', '420'), ('num 21', '21'), ('just text', None)])
+def test_get_clipboard_content(copied, expected):
+    # run powershell -h to know how to pass args to powershell via cmd. 
+    cmd = 'powershell -Command "& {Set-Clipboard -Value \'{_}\'}'.replace('_', copied)
+    subprocess.run(cmd, shell=True)
+    assert get_clipboard_content() == expected 
+
+
+def test_purge_clipboard_contents():
+    copied = call_clipboard_cmd()
+    assert copied
+    purge_clipboard_contents()
+    assert not call_clipboard_cmd()
+    
