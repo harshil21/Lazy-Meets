dd944db30c2d58a5c44f44c1c4974c8973aa9107
Harshil <ilovebhagwan@gmail.com>
2021-09-14 00:12:33 +0400

    add screenshot function

---
+++ helpers/screengrab.py
@@ -0,0 +1,7 @@
+import pyautogui as gui
+
+
+def grab_screenshot(string_region: tuple) -> None:
+    """Saves a screenshot of the current participant list string so we can do OCR only on that."""
+    left, top, width, height = string_region
+    gui.screenshot('ocr_participant.png', region=(left, top, width + 30, height))  # add 30 pixels to include number
